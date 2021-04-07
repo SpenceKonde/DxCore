3015f30425cfb71a0c82ec9a9a9a7d0828c3da13
Spence Konde <spencekonde@gmail.com>
2021-04-07 00:22:40 -0400

    stylecheck

--- megaavr/libraries/DxCore/src/DxCore.h
+++ megaavr/libraries/DxCore/src/DxCore.h
@@ -86,7 +86,7 @@ bool setTCA0MuxByPort(uint8_t port) {
   if (port < 7) {
     TCA0.SPLIT.CTRLB = 0; //disconnect
     uint8_t base_pin = digitalPortToPinZero(port);
-    uint8_t max_pin = min((base_pin + 6 > NUM_DIGITAL_PINS ? NUM_DIGITAL_PINS : base_pin+6), digitalPortToPinZero(port + 1)) - 1;
+    uint8_t max_pin = min((base_pin + 6 > NUM_DIGITAL_PINS ? NUM_DIGITAL_PINS : base_pin + 6), digitalPortToPinZero(port + 1)) - 1;
     for (byte i = base_pin; i < (min(max_pin, base_pin+6); i++)) {
       turnOffPWM(i);
     }
@@ -121,7 +121,7 @@ bool setTCA1MuxByPort(uint8_t port, bool takeover_only_ports_ok = false) {
   // AND with group mask cuts off the unwanted low bit leaving us with the 2 high bits which is what we care about
   TCA1.SPLIT.CTRLB = 0; //disconnect all PWM channels
   uint8_t base_pin = digitalPortToPinZero(port);
-  uint8_t max_pin = min((base_pin + 6 > NUM_DIGITAL_PINS ? NUM_DIGITAL_PINS : base_pin+6), digitalPortToPinZero(port + 1)) - 1;
+  uint8_t max_pin = min((base_pin + 6 > NUM_DIGITAL_PINS ? NUM_DIGITAL_PINS : base_pin + 6), digitalPortToPinZero(port + 1)) - 1;
   for (byte i = base_pin; i < max_pin; i++) {
     turnOffPWM(i);
   }
@@ -133,11 +133,11 @@ bool setTCA1MuxByPin(uint8_t pin, bool takeover_only_ports_ok = false) {
   uint8_t port = digitalPinToPort(pin);
   uint8_t bit_mask = digitalPinToBitMask(pin);
   #if defined(DB_64_PINS)
-  // AVR128DB, AVR64DB work with the high MUX options
+  // AVR128DB, AVR64DB work with the high MUX options *INDENT-OFF*
     if (((port == 1 || port == 6) && (bit_mask & 0x3F)) || (bit_mask & 0x70)) {
   #else  // AVR128DA64 definitely do not work. AVR64DA64 untested.
     if (((port == 1) && (bit_mask & 0x3F)) || (bit_mask & 0x70)) {
-  #endif // And those are the only 4 parts in the product line for which those pins exist.
+  #endif // And those are the only 4 parts in the product line for which those pins exist. *INDENT-ON*
     // PORTB and PORTG have full-service TCA1 (well, not PG on the 128DA63 up to at least the A8 die
     // rev). for those, bit_mask will be 0x01, 0x02, 0x04, 0x08, 0x10, or 0x20 - but not 0x40 or
     // 0x80. Hence test against 0x3F works. For the others, it is either 0x10, 0x20, or 0x40 so
