efe1ab5e678481406e64ee7b96887b29439b1f70
Spence Konde <spencekonde@gmail.com>
2021-11-08 17:08:09 -0500

    jumping through more hoops

---
+++ megaavr/extras/ci/test-sketch/da/dummysketch/dummysketch.ino
@@ -0,0 +1,9 @@
+/* test fails entirely if any one loction in the list doesn't have a sketch, so we have to provide 
+ * a dummy sketch for lists which might otherwise be empty. 
+ */
+
+void setup() {
+}
+
+void loop() {
+}
