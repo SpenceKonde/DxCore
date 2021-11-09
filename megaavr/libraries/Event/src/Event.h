8460d737b6e493c71064aa6060146c8ad76c418f
Spence Konde <spencekonde@gmail.com>
2021-11-09 02:12:08 -0500

    Newer yet still incomplete revamped event.h

--- megaavr/libraries/Event/src/Event.h
+++ megaavr/libraries/Event/src/Event.h
@@ -34,10 +34,18 @@ namespace gen {
     tca0_cmp1     = 0x85,
     tca0_cmp2     = 0x86,
     tcb0_capt     = 0xA0,
+    tcb0          = 0xA0,
     tcb1_capt     = 0xA2,
+    tcb1          = 0xA2,
     tcb2_capt     = 0xA4,
+    tcb2          = 0xA4,
 #   if defined(TCB3)
     tcb3_capt     = 0xA6,
+    tcb3          = 0xA6,
+#   endif
+#   if defined(TCB4)
+    tcb4_capt      = 0xA8,
+    tcb4           = 0xA8,
 #   endif
 #if defined(__AVR_DA__) || defined(__AVR_DB__)
     ccl4_out      = 0x14,
@@ -63,9 +71,6 @@ namespace gen {
     tca1_cmp1     = 0x8D,
     tca1_cmp2     = 0x8E,
 #   endif
-#   if defined(TCB4)
-    tcb4_capt     = 0xA8,
-#   endif
     tcb0_ovf      = 0xA1,
     tcb1_ovf      = 0xA3,
     tcb2_ovf      = 0xA5,
@@ -80,8 +85,10 @@ namespace gen {
     tcd0_cmpbset  = 0xB2,
     tcd0_progev   = 0xB3,
 #endif // defined(__AVR_DA__) || defined(__AVR_DB__)
-#if defined(__AVR_DB__)
+#if defined(MVIO)
     mvio_ok       = 0x05,
+#endif
+#if defined(__AVR_DB__)
     opamp0_ready  = 0x34,
     opamp1_ready  = 0x35,
 #   if defined (OPAMP2)
@@ -374,44 +381,44 @@ namespace gen9 {
 namespace user {
   enum user_t : uint8_t {
 #if defined(__AVR_ATmegax08__) || defined(__AVR_ATmegax09__)
-      ccl0_event_a   = 0x00,
-      ccl0_event_b   = 0x01,
-      ccl1_event_a   = 0x02,
-      ccl1_event_b   = 0x03,
-      ccl2_event_a   = 0x04,
-      ccl2_event_b   = 0x05,
-      ccl3_event_a   = 0x06,
-      ccl3_event_b   = 0x07,
-      adc0_start     = 0x08,
-      evouta_pin_pa2 = 0x09,
-#     if defined(__AVR_ATmegax09__)
-      evoutb_pin_pb2 = 0x0A,
-#     endif
-      evoutc_pin_pc2 = 0x0B,
-      evoutd_pin_pd2 = 0x0C,
-#     if defined(__AVR_ATmegax09__)
-      evoute_pin_pe2 = 0x0D,
-#     endif
-      evoutf_pin_pf2 = 0x0E,
-      usart0_irda    = 0x0F,
-      usart1_irda    = 0x10,
-      usart2_irda    = 0x11,
-#     if defined(USART3)
-      usart3_irda    = 0x12,
-#     endif
-      tca0           = 0x13,
-      tcb0           = 0x14,
-      tcb1           = 0x15,
-      tcb2           = 0x16,
-#     if defined(TCB3)
-      tcb3           = 0x17,
-#     endif
-      // "Unofficial" user generators. Uses EVOUT, but swaps the output pin using PORTMUX
-      evouta_pin_pa7 = 0x89,
-#     if defined(__AVR_ATmegax09__)
-      evoutc_pin_pc7 = 0x8B,
-#     endif
-      evoutd_pin_pd7 = 0x8C,
+    ccl0_event_a   = 0x00,
+    ccl0_event_b   = 0x01,
+    ccl1_event_a   = 0x02,
+    ccl1_event_b   = 0x03,
+    ccl2_event_a   = 0x04,
+    ccl2_event_b   = 0x05,
+    ccl3_event_a   = 0x06,
+    ccl3_event_b   = 0x07,
+    adc0_start     = 0x08,
+    evouta_pin_pa2 = 0x09,
+#if defined(__AVR_ATmegax09__)
+    evoutb_pin_pb2 = 0x0A,
+#endif
+    evoutc_pin_pc2 = 0x0B,
+    evoutd_pin_pd2 = 0x0C,
+#if defined(__AVR_ATmegax09__)
+    evoute_pin_pe2 = 0x0D,
+  #endif
+    evoutf_pin_pf2 = 0x0E,
+    usart0_irda    = 0x0F,
+    usart1_irda    = 0x10,
+    usart2_irda    = 0x11,
+#if defined(USART3)
+    usart3_irda    = 0x12,
+#endif
+    tca0           = 0x13,
+    tcb0           = 0x14,
+    tcb1           = 0x15,
+    tcb2           = 0x16,
+#if defined(TCB3)
+    tcb3           = 0x17,
+#endif
+    // "Unofficial" user generators. Uses EVOUT, but swaps the output pin using PORTMUX
+    evouta_pin_pa7 = 0x89,
+#if defined(__AVR_ATmegax09__)
+    evoutc_pin_pc7 = 0x8B,
+#endif
+    evoutd_pin_pd7 = 0x8C,
 #endif // defined(__AVR_ATmegax08__) || defined(__AVR_ATmegax09__)
 #if defined(__AVR_DA__)
       ccl0_event_a   = 0x00,
@@ -458,21 +465,27 @@ namespace user {
       usart5_irda    = 0x1A,
 #     endif
       tca0_cnt_a     = 0x1B,
+      tca0_cnt       = 0x1B,
       tca0_cnt_b     = 0x1C,
       tca1_cnt_a     = 0x1D,
       tca1_cnt_b     = 0x1E,
       tcb0_capt      = 0x1F,
+      tcb0           = 0x1F,
       tcb0_cnt       = 0x20,
       tcb1_capt      = 0x21,
+      tcb1           = 0x21,
       tcb1_cnt       = 0x22,
       tcb2_capt      = 0x23,
+      tcb2           = 0x23,
       tcb2_cnt       = 0x24,
 #     if defined(TCB3)
       tcb3_capt      = 0x25,
+      tcb4           = 0x25,
       tcb3_cnt       = 0x26,
 #     endif
-#     if defined(TCB3)
+#     if defined(TCB4)
       tcb4_capt      = 0x27,
+      tcb4           = 0x27,
       tcb4_cnt       = 0x28,
 #     endif
       tcd0_in_a      = 0x29,
@@ -495,110 +508,185 @@ namespace user {
 #     endif
 #endif // defined(__AVR_DA__)
 #if defined(__AVR_DB__)
-      ccl0_event_a   = 0x00,
-      ccl0_event_b   = 0x01,
-      ccl1_event_a   = 0x02,
-      ccl1_event_b   = 0x03,
-      ccl2_event_a   = 0x04,
-      ccl2_event_b   = 0x05,
-      ccl3_event_a   = 0x06,
-      ccl3_event_b   = 0x07,
-#     if defined(LUT4)
-      ccl4_event_a   = 0x08,
-      ccl4_event_b   = 0x09,
-      ccl5_event_a   = 0x0A,
-      ccl5_event_b   = 0x0B,
-#     endif
-      adc0_start     = 0x0C,
-      evouta_pin_pa2 = 0x0D,
-#     if (defined(Dx_64_PINS)||defined(Dx_48_PINS))
-      evoutb_pin_pb2 = 0x0E,
-#     endif
-      evoutc_pin_pc2 = 0x0F,
-      evoutd_pin_pd2 = 0x10,
-#     if (defined(Dx_64_PINS)||defined(Dx_48_PINS))
-      evoute_pin_pe2 = 0x11,
-#     endif
-#     if (!defined(Dx_28_PINS))
-      evoutf_pin_pf2 = 0x12,
-#     endif
-#     if defined(Dx_64_PINS)
-      evoutg_pin_pg2 = 0x13,
-#     endif
-      usart0_irda    = 0x14,
-      usart1_irda    = 0x15,
-      usart2_irda    = 0x16,
-#     if defined(USART3)
-      usart3_irda    = 0x17,
-#     endif
-#     if defined(USART4)
-      usart4_irda    = 0x18,
-#     endif
-#     if defined(USART5)
-      usart5_irda    = 0x19,
-#     endif
-      tca0_cnt_a     = 0x1A,
-      tca0_cnt_b     = 0x1B,
-      tca1_cnt_a     = 0x1C,
-      tca1_cnt_b     = 0x1D,
-      tcb0_capt      = 0x1E,
-      tcb0_cnt       = 0x1F,
-      tcb1_capt      = 0x20,
-      tcb1_cnt       = 0x21,
-      tcb2_capt      = 0x22,
-      tcb2_cnt       = 0x23,
-#     if defined(TCB3)
-      tcb3_capt      = 0x24,
-      tcb3_cnt       = 0x25,
-#     endif
-#     if defined(TCB3)
-      tcb4_capt      = 0x26,
-      tcb4_cnt       = 0x27,
-#     endif
-      tcd0_in_a      = 0x28,
-      tcd0_in_b      = 0x29,
-      opamp0_enable  = 0x2A,
-      opamp0_disable = 0x2B,
-      opamp0_dump    = 0x2C,
-      opamp0_drive   = 0x2D,
-      opamp1_enable  = 0x2E,
-      opamp1_disable = 0x2F,
-      opamp1_dump    = 0x30,
-      opamp1_drive   = 0x31,
-#     if defined(OPAMP2)
-      opamp2_enable  = 0x32,
-      opamp2_disable = 0x33,
-      opamp2_dump    = 0x34,
-      opamp2_drive   = 0x35,
-#     endif
-      // "Unofficial" user generators. Uses EVOUT, but swaps the output pin using PORTMUX
-      evouta_pin_pa7 = 0x8D,
-#     if (defined(Dx_64_PINS))
-      evoutb_pin_pb7 = 0x8E,
-#     endif
-#     if (defined(Dx_48_PINS)||defined(Dx_64_PINS))
-      evoutc_pin_pc7 = 0x8F,
-#     endif
-      evoutd_pin_pd7 = 0x90,
-#     if (defined(Dx_64_PINS))
-      evoutb_pin_pe7 = 0x91,
-#     endif
-      // evoutf_pin_pf7 = 0x92, never available on DB
-#     if (defined(Dx_64_PINS))
-      evoutb_pin_pg7 = 0x93,
-#     endif
+    ccl0_event_a   = 0x00,
+    ccl0_event_b   = 0x01,
+    ccl1_event_a   = 0x02,
+    ccl1_event_b   = 0x03,
+    ccl2_event_a   = 0x04,
+    ccl2_event_b   = 0x05,
+    ccl3_event_a   = 0x06,
+    ccl3_event_b   = 0x07,
+#if defined(LUT4)
+    ccl4_event_a   = 0x08,
+    ccl4_event_b   = 0x09,
+    ccl5_event_a   = 0x0A,
+    ccl5_event_b   = 0x0B,
+#endif
+    adc0_start     = 0x0C,
+    evouta_pin_pa2 = 0x0D,
+#if defined(Dx_64_PINS) || defined(Dx_48_PINS)
+    evoutb_pin_pb2 = 0x0E,
+#endif
+    evoutc_pin_pc2 = 0x0F,
+    evoutd_pin_pd2 = 0x10,
+#if defined(Dx_64_PINS) || defined(Dx_48_PINS)
+    evoute_pin_pe2 = 0x11,
+#endif
+#if !defined(Dx_28_PINS)
+    evoutf_pin_pf2 = 0x12,
+#endif
+#if defined(Dx_64_PINS)
+    evoutg_pin_pg2 = 0x13,
+  #endif
+    usart0_irda    = 0x14,
+    usart1_irda    = 0x15,
+    usart2_irda    = 0x16,
+#if defined(USART3)
+    usart3_irda    = 0x17,
+#endif
+#if defined(USART4)
+    usart4_irda    = 0x18,
+#endif
+#if defined(USART5)
+    usart5_irda    = 0x19,
+#endif
+    tca0_cnt_a     = 0x1A,
+    tca0_cnt_b     = 0x1B,
+    tca1_cnt_a     = 0x1C,
+    tca1_cnt_b     = 0x1D,
+    tcb0_capt      = 0x1E,
+    tcb0_cnt       = 0x1F,
+    tcb1_capt      = 0x20,
+    tcb1_cnt       = 0x21,
+    tcb2_capt      = 0x22,
+    tcb2_cnt       = 0x23,
+#if defined(TCB3)
+    tcb3_capt      = 0x24,
+    tcb3_cnt       = 0x25,
+#endif
+#if defined(TCB4)
+    tcb4_capt      = 0x26,
+    tcb4_cnt       = 0x27,
+#endif
+    tcd0_in_a      = 0x28,
+    tcd0_in_b      = 0x29,
+    opamp0_enable  = 0x2A,
+    opamp0_disable = 0x2B,
+    opamp0_dump    = 0x2C,
+    opamp0_drive   = 0x2D,
+    opamp1_enable  = 0x2E,
+    opamp1_disable = 0x2F,
+    opamp1_dump    = 0x30,
+    opamp1_drive   = 0x31,
+#if defined(OPAMP2)
+    opamp2_enable  = 0x32,
+    opamp2_disable = 0x33,
+    opamp2_dump    = 0x34,
+    opamp2_drive   = 0x35,
+#endif
+    // "Unofficial" user generators. Uses EVOUT, but swaps the output pin using PORTMUX
+    evouta_pin_pa7 = 0x8D,
+#if defined(Dx_64_PINS)
+    evoutb_pin_pb7 = 0x8E,
+#endif
+#if defined(Dx_48_PINS) || defined(Dx_64_PINS)
+    evoutc_pin_pc7 = 0x8F,
+#endif
+    evoutd_pin_pd7 = 0x90,
+#if defined(Dx_64_PINS)
+    evoutb_pin_pe7 = 0x91,
+#endif
+#if defined(Dx_64_PINS)
+    evoutb_pin_pg7 = 0x93,
+#endif
 #endif // defined(__AVR_DB__)
   };
 };
+
 
 class Event {
   public:
     Event(uint8_t channel_num, volatile uint8_t &channel_addr);
     uint8_t get_channel_number();
-    static int8_t get_user_channel(user::user_t event_user);
-    void set_generator(uint8_t generator);
+    static Event& get_channel(uint8_t channel_number);
+    static Event& get_generator_channel(uint8_t generator);
     uint8_t get_generator();
+    void set_generator(gen::generator_t generator);
+    void set_generator(uint8_t pin_number);
+
+    #if defined(EVSYS_CHANNEL0)
+      void set_generator(gen0::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+    #if defined(EVSYS_CHANNEL1)
+      void set_generator(gen1::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+    #if defined(EVSYS_CHANNEL2)
+      void set_generator(gen2::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+    #if defined(EVSYS_CHANNEL3)
+      void set_generator(gen3::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+    #if defined(EVSYS_CHANNEL4)
+      void set_generator(gen4::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+    #if defined(EVSYS_CHANNEL5)
+      void set_generator(gen5::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+    #if defined(EVSYS_CHANNEL6)
+      void set_generator(gen6::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+    #if defined(EVSYS_CHANNEL7)
+      void set_generator(gen7::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+    #if defined(EVSYS_CHANNEL8)
+      void set_generator(gen8::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+    #if defined(EVSYS_CHANNEL9)
+      void set_generator(gen9::generator_t generator) { set_generator((gen::generator_t)generator); }
+    #endif
+
+
+    static Event& assign_generator_pin(uint8_t pin_number);
+    static Event& assign_generator_pin(uint8_t port,uint8_t port_pin);
+    static Event& assign_generator(gen::generator_t event_generator, uint8_t ch = 255);
+
+    #if defined(EVSYS_CHANNEL0)
+      static Event& assign_generator(gen0::generator_t generator) { set_generator((gen::generator_t)generator, 0); }
+    #endif
+    #if defined(EVSYS_CHANNEL1)
+      static Event& assign_generator(gen1::generator_t generator) { set_generator((gen::generator_t)generator, 1); }
+    #endif
+    #if defined(EVSYS_CHANNEL2)
+      static Event& assign_generator(gen2::generator_t generator) { set_generator((gen::generator_t)generator, 2); }
+    #endif
+    #if defined(EVSYS_CHANNEL3)
+      static Event& assign_generator(gen3::generator_t generator) { set_generator((gen::generator_t)generator, 3); }
+    #endif
+    #if defined(EVSYS_CHANNEL4)
+      static Event& assign_generator(gen4::generator_t generator) { set_generator((gen::generator_t)generator, 4); }
+    #endif
+    #if defined(EVSYS_CHANNEL5)
+      static Event& assign_generator(gen5::generator_t generator) { set_generator((gen::generator_t)generator, 5); }
+    #endif
+    #if defined(EVSYS_CHANNEL6)
+      static Event& assign_generator(gen6::generator_t generator) { set_generator((gen::generator_t)generator, 6); }
+    #endif
+    #if defined(EVSYS_CHANNEL7)
+      static Event& assign_generator(gen7::generator_t generator) { set_generator((gen::generator_t)generator, 7); }
+    #endif
+    #if defined(EVSYS_CHANNEL8)
+      static Event& assign_generator(gen8::generator_t generator) { set_generator((gen::generator_t)generator, 8); }
+    #endif
+    #if defined(EVSYS_CHANNEL9)
+      static Event& assign_generator(gen9::generator_t generator) { set_generator((gen::generator_t)generator, 9); }
+    #endif
+
+
+
+    static int8_t get_user_channel_number(user::user_t event_user);
+    static Event& get_user_channel(user::user_t event_user);
     void set_user(user::user_t event_user);
+    int8_t set_user_pin(uint8_t pin_number);
     static void clear_user(user::user_t event_user);
     void soft_event();
     void start(bool state = true);
@@ -640,5 +728,4 @@ class Event {
 #if defined(EVSYS_CHANNEL9)
   extern Event Event9;
 #endif
-
 #endif // EVENT_H
