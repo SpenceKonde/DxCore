bcac462f98a03341b24d673bbdf047a758b41437
Spence Konde <spencekonde@gmail.com>
2021-11-10 15:12:36 -0500

    Fix issue with an example

--- megaavr/libraries/tinyNeoPixel/examples/buttoncycler/buttoncycler.ino
+++ megaavr/libraries/tinyNeoPixel/examples/buttoncycler/buttoncycler.ino
@@ -3,36 +3,162 @@
 
 #include <tinyNeoPixel.h>
 
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN            3
+#define BUTTON_PIN   2    // Digital IO pin connected to the button.  This will be
+// driven with a pull-up resistor so the switch should
+// pull the pin to ground momentarily.  On a high -> low
+// transition the button press logic will execute.
 
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      16
+#define PIXEL_PIN    3    // Digital IO pin connected to the NeoPixels
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
+#define PIXEL_COUNT 16
+
+
+// Parameter 1 = number of pixels in strip,  neopixel stick has 8
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
+
 tinyNeoPixel pixels = tinyNeoPixel(NUMPIXELS, PIN, NEO_GRB);
 
-int delayval = 500; // delay for half a second
+bool oldState = HIGH;
+int showType = 0;
 
 void setup() {
-
-  pixels.begin(); // This initializes the NeoPixel library.
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
+  // Get current button state.
+  bool newState = digitalRead(BUTTON_PIN);
 
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-
-  for (int i = 0; i < NUMPIXELS; i++) {
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0)); // Moderately bright green color.
-
-    pixels.show(); // This sends the updated pixel color to the hardware.
-
-    delay(delayval); // Delay for a period of time (in milliseconds).
-
+  // Check if state changed from high to low (button press).
+  if (newState == LOW && oldState == HIGH) {
+    // Short delay to debounce button.
+    delay(20);
+    // Check if button is still low after debounce.
+    newState = digitalRead(BUTTON_PIN);
+    if (newState == LOW) {
+      showType++;
+      if (showType > 9) {
+        showType = 0;
+      }
+      startShow(showType);
+    }
   }
+
+  // Set the last button state to the old state.
+  oldState = newState;
+}
+
+void startShow(int i) {
+  switch (i) {
+    case 0: colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
+      break;
+    case 1: colorWipe(strip.Color(255, 0, 0), 50);  // Red
+      break;
+    case 2: colorWipe(strip.Color(0, 255, 0), 50);  // Green
+      break;
+    case 3: colorWipe(strip.Color(0, 0, 255), 50);  // Blue
+      break;
+    case 4: theaterChase(strip.Color(127, 127, 127), 50); // White
+      break;
+    case 5: theaterChase(strip.Color(127,   0,   0), 50); // Red
+      break;
+    case 6: theaterChase(strip.Color(0,   0, 127), 50);   // Blue
+      break;
+    case 7: rainbow(20);
+      break;
+    case 8: rainbowCycle(20);
+      break;
+    case 9: theaterChaseRainbow(50);
+      break;
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
+    for (int q = 0; q < 3; q++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, Wheel((i + j) % 255)); //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
