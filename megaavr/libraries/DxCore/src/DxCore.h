f901f29d4d4c36d34d4ec118d310d8abe1123434
Spence Konde <spencekonde@gmail.com>
2022-02-05 14:47:59 -0500

    fix MVIO issue correctly
    formatting bullshit

--- megaavr/libraries/DxCore/src/DxCore.h
+++ megaavr/libraries/DxCore/src/DxCore.h
@@ -57,21 +57,21 @@ int8_t getMVIOVoltage();
 
 
 #if defined(MVIO) && defined(HAS_64_PINS)
-  int8_t setMAX38903Voltage(uint8_t setting);
-  #define REG_OFF 0xFF
-  #define REG_1V2 0b01000100
-  #define REG_1V5 0b10001000
-  #define REG_1V8 0b01000000
-  #define REG_2V5 0b00000000
-  #define REG_3V0 0b11000000
-  #define REG_3V1 0b11000100
-  #define REG_3V3 0b10000000
-  #define REG_4V0 0b11001000
-  #define REG_5V0 0b11001100
+int8_t setMAX38903Voltage(uint8_t setting);
+#define REG_OFF 0xFF
+#define REG_1V2 0b01000100
+#define REG_1V5 0b10001000
+#define REG_1V8 0b01000000
+#define REG_2V5 0b00000000
+#define REG_3V0 0b11000000
+#define REG_3V1 0b11000100
+#define REG_3V3 0b10000000
+#define REG_4V0 0b11001000
+#define REG_5V0 0b11001100
 #endif
 
 #ifdef __cplusplus
-  extern }
+}
 #endif
 
 // passed a port number (eg, digitalPinToPort() from pin, or number 0-6)
