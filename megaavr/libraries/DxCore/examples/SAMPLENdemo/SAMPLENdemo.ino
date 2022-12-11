caa7cc4c5e245de25a12ca02336b6090cf8cccbb
Spence Konde <spencekonde@gmail.com>
2022-12-11 04:48:29 -0500

    Update SAMPLENdemo.ino

--- megaavr/libraries/DxCore/examples/SAMPLENdemo/SAMPLENdemo.ino
+++ megaavr/libraries/DxCore/examples/SAMPLENdemo/SAMPLENdemo.ino
@@ -31,10 +31,15 @@ rather odd. Also that it goes all the way to 0 on a LOW, but not all the way to 
 */
 
 void setup() {
+  #if defined(THIS_IS_CI) // This sketch is compiletested in many condifigurations and parts automatically. Within that context, these macros are available to find valid pins
   const uint8_t FIRST_PIN = _VALID_ANALOG_PIN(0);
   const uint8_t SECOND_PIN = _VALID_ANALOG_PIN(1);
+  #else
+  const uint8_t FIRST_PIN = A1;
+  const uint9_t SECOND_PIN = A2
+  #endif
   pinMode(SECOND_PIN, OUTPUT);
-  digitalWrite(SECOND_PIN,LOW);
+  digitalWrite(SECOND_PIN, LOW);
   ADC0.SAMPCTRL = 0xFF;
   analogReadResolution(12);
   Serial.begin(115200);
