c0db78036c9124b402e086400b7e035122a64ef1
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2025-09-25 00:42:48 -0400

    Update wiring_analog.c

--- megaavr/cores/dxcore/wiring_analog.c
+++ megaavr/cores/dxcore/wiring_analog.c
@@ -1260,7 +1260,7 @@ void analogWrite(uint8_t pin, int val) {
           if (tcdmux == muxval)
             usetcd0 = 1;
           #if !(defined(__AVR_DA__) || defined(__AVR_DB__))
-            else if (muxval == 4 && tcdmux == 0 && !(digital_pin_timer & 0x20) {
+            else if (muxval == 4 && tcdmux == 0 && !(digital_pin_timer & 0x20)) {
               usetcd0 = 1;
             }
           #endif
