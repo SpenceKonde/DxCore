3080cbd6857596b12865d1acf3aa9bf6ce74371d
wke67 <wke67@t-online.de>
2026-04-13 18:06:12 +0200

    ci test for pins_arduino.h

---
+++ megaavr/extras/ci/test-sketch/all/macros/macros.ino
@@ -0,0 +1,19 @@
+/* test for macros from pins_arduino.h
+*/
+
+void setup() {
+  volatile int i,p;
+
+  for( p=0; p<64; p++) {
+    i=digitalPinToAnalogInput(p);
+    i+=analogChannelToDigitalPin(p);
+    i+=analogInputToDigitalPin(p);
+    i+=digitalOrAnalogPinToDigital(p);
+    i+=portToPinZero(p);
+    i+=digitalPinHasPWMTCB(p);
+    i+=digitalPinHasPWM(p);
+  }
+}
+
+void loop() {
+}
